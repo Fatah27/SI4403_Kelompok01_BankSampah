60e9a9727b4067659986fe451d1bc7ed9ad7a4fa
diki1211 <dikiilham1@gmail.com>
2023-01-04 03:15:57 +0700

    login

--- Tubes/routes/web.php
+++ Tubes/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/' , [\App\Http\Controllers\IndexController::class , 'index'])->name('index');
 
