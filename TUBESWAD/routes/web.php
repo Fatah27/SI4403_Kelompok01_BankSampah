eba8cdd17b8d6bb8fc2876dfa579fdec92521cb8
FatahKhair <32662195+Fatah27@users.noreply.github.com>
2023-01-04 02:13:32 +0700

    routes

--- TUBESWAD/routes/web.php
+++ TUBESWAD/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/' , [\App\Http\Controllers\IndexController::class , 'index'])->name('index');
+
